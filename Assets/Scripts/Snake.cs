2e31925fb22283c5314374c583b0a99455239798
Blossantrape <tolik.hamer@gmail.com>
2023-05-15 17:50:43 +0300

    minor fix

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,15 +3,44 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private Vector2Int gridPosition;
-
+    private Vector2Int gridMoveDirection; // Направление змейки.
+    private Vector3Int gridPosition; // Позиция змейки.
+    private float gridMoveTimer; // Время для автоматического премещения змейки.
+    private float gridMoveTimerMax; // Её максимальное значение.
+    
     private void Awake()
     {
-        gridPosition = new Vector2Int(10, 10);
+        gridPosition = new Vector3Int(10, 10, 1);
+        gridMoveTimerMax = 1f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
     }
 
     private void Update()
     {
-        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gridPosition.y += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gridPosition.y -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gridPosition.x -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gridPosition.x += 1;
+        }
+
+        gridMoveTimer = Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax)
+        {
+            
+        }
+        
+        transform.position = new Vector3(gridPosition.x, gridPosition.y, gridPosition.z);
     }
 }
