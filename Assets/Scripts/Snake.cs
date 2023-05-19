8d9337737705265ee9c8178edf933c00f37eadc9
Blossantrape <tolik.hamer@gmail.com>
2023-05-19 17:51:10 +0300

    rotation of sprite

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,44 +3,85 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private Vector2Int gridMoveDirection; // Направление змейки.
+    private Vector3Int gridMoveDirection; // Направление змейки.
     private Vector3Int gridPosition; // Позиция змейки.
     private float gridMoveTimer; // Время для автоматического премещения змейки.
     private float gridMoveTimerMax; // Её максимальное значение.
     
-    private void Awake()
+    private void Awake() 
     {
-        gridPosition = new Vector3Int(10, 10, 1);
-        gridMoveTimerMax = 1f;
-        gridMoveTimer = gridMoveTimerMax;
-        gridMoveDirection = new Vector2Int(1, 0);
+        gridPosition = new Vector3Int(10, 10, 1); // Позция змейки.
+        gridMoveTimerMax = .5f; // Интервал движения.
+        gridMoveTimer = gridMoveTimerMax; // Так надо.
+        gridMoveDirection = new Vector3Int(1, 0, 0); // Векторное управление змеи, вправо.
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        HandleInput();
+        HandleGrindMovement();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) // Управление стрелками.
         {
-            gridPosition.y += 1;
+            if (gridMoveDirection.y != -1) // Не позволяет поворачиваться на 180 градусов.
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = +1;
+            }
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            gridPosition.y -= 1;
+            if (gridMoveDirection.y != +1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            gridPosition.x -= 1;
+            if (gridMoveDirection.x != +1)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+            
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            gridPosition.x += 1;
+            if (gridMoveDirection.x != -1)
+            {
+                gridMoveDirection.x = +1;
+                gridMoveDirection.y = 0;
+            }
         }
+    }
 
-        gridMoveTimer = Time.deltaTime;
-        if (gridMoveTimer >= gridMoveTimerMax)
+    private void HandleGrindMovement()
+    {
+        gridMoveTimer += Time.deltaTime; // Обновление таймера каждый кадр.
+        if (gridMoveTimer >= gridMoveTimerMax) // Если время >= максимального, то цикл выполняется.
         {
+            gridPosition += gridMoveDirection; // Позиция + движение в сторону.
+            gridMoveTimer -= gridMoveTimerMax; // Не понял зачем.
             
+            transform.position = new Vector3
+                (gridPosition.x, gridPosition.y, gridPosition.z); // Изменение позиции змейки.
+            transform.eulerAngles = new Vector3
+                (0, 0, GetAngleFromVector(gridMoveDirection) -90); // Изменение направления спрайта
+                                            // по углу эйлера в взависимости от направления движения по Z.
         }
-        
-        transform.position = new Vector3(gridPosition.x, gridPosition.y, gridPosition.z);
+    }
+
+    private float GetAngleFromVector(Vector3Int dir) // Метор определяющий направление спрайта.
+    {
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n > 0)
+        {
+            n += 360;
+        }
+        return n;
     }
 }
