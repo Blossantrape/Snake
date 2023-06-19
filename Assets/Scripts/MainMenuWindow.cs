86d0229b01c8c0805cac26be2a61dba41aeb2d8d
Blossantrape <tolik.hamer@gmail.com>
2023-06-19 22:09:55 +0300

    minor fix.
    делаю паузу.

--- Assets/Scripts/MainMenuWindow.cs
+++ Assets/Scripts/MainMenuWindow.cs
@@ -1,15 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuWindow : MonoBehaviour
 {
+    private enum Sub
+    {
+        MainMenu,
+        HowToPlay,
+    }
     private void Awake()
     {
-        /*// кастыль ебаный
-        transform.Find("playBtn"); //clickfund = Loader.Load(Loader.Scene.GameScene);
+        transform.Find("buttonsMainMenu").GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        transform.Find("howToPlayInside").GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        // Тут мы в дочерни ищем кнопку и через компонент кнопки по клику вызываем нужный функианал.
+        transform.Find("buttonsMainMenu").Find("playBtn").GetComponent<Button>().onClick.AddListener(() => Loader.Load(Loader.Scene.GameScene));
         
-        transform.Find("quitBtn"); //clickfund = Application.Quit();*/
+        transform.Find("buttonsMainMenu").Find("howToPlayBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.HowToPlay));
         
+        transform.Find("buttonsMainMenu").Find("quitBtn").GetComponent<Button>().onClick.AddListener(() => Application.Quit());
         
+        transform.Find("howToPlayInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.MainMenu));
+        
+        ShowSub(Sub.MainMenu);
+    }
+
+    private void ShowSub(Sub sub)
+    {
+        transform.Find("buttonsMainMenu").gameObject.SetActive(false);
+        transform.Find("howToPlayInside").gameObject.SetActive(false);
+
+        switch (sub)
+        {
+            case Sub.MainMenu:
+                transform.Find("howToPlayInside").gameObject.SetActive(false);
+                transform.Find("buttonsMainMenu").gameObject.SetActive(true);
+                break;
+            case Sub.HowToPlay:
+                transform.Find("buttonsMainMenu").gameObject.SetActive(false);
+                transform.Find("howToPlayInside").gameObject.SetActive(true);
+                break;
+        }
     }
 }
