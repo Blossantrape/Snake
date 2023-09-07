1962a3f9e3769f2376354e005ecb975468111f9a
Blossantrape <tolik.hamer@gmail.com>
2023-09-07 17:17:13 +0300

    v 1.4
    Добавил ассет для анимаций, создал анимации в главном меню.

--- Assets/Scripts/UI/MainMenuWindow.cs
+++ Assets/Scripts/UI/MainMenuWindow.cs
@@ -7,11 +7,18 @@ namespace UI
 {
     public class MainMenuWindow : MonoBehaviour
     {
+        [SerializeField] private Transform _howToPlayInside;
+        [SerializeField] private Transform _settingsInside;
+        [SerializeField] private Transform _snakeText;
+        [SerializeField] private CanvasGroup _background;
+        //private float _timer;
+        
         private enum Sub
         {
             MainMenu,
             HowToPlay,
             Settings,
+            Back,
         }
         private void Awake()
         {
@@ -34,9 +41,9 @@ namespace UI
             transform.Find("buttonsMainMenu").Find("quitBtn").GetComponent<Button>().onClick.AddListener(() => SoundManager.PlaySound(SoundManager.Sound.ButtonClick));
         
             // Back Buttons
-            transform.Find("howToPlayInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.MainMenu));
+            transform.Find("howToPlayInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.Back));
             transform.Find("howToPlayInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => SoundManager.PlaySound(SoundManager.Sound.ButtonClick));
-            transform.Find("settingsInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.MainMenu));
+            transform.Find("settingsInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => ShowSub(Sub.Back));
             transform.Find("settingsInside").Find("backBtn").GetComponent<Button>().onClick.AddListener(() => SoundManager.PlaySound(SoundManager.Sound.ButtonClick));
         
             ShowSub(Sub.MainMenu);
@@ -45,22 +52,43 @@ namespace UI
         private void ShowSub(Sub sub)
         {
             transform.Find("buttonsMainMenu").gameObject.SetActive(false);
-            transform.Find("howToPlayInside").gameObject.SetActive(false);
-            transform.Find("settingsInside").gameObject.SetActive(false);
+            //transform.Find("howToPlayInside").gameObject.SetActive(false);
+            //transform.Find("settingsInside").gameObject.SetActive(false);
 
             switch (sub)
             {
                 case Sub.MainMenu:
                     transform.Find("howToPlayInside").gameObject.SetActive(false);
+                    transform.Find("settingsInside").gameObject.SetActive(false);
                     transform.Find("buttonsMainMenu").gameObject.SetActive(true);
                     break;
                 case Sub.HowToPlay:
-                    transform.Find("buttonsMainMenu").gameObject.SetActive(false);
+                    transform.Find("buttonsMainMenu").gameObject.SetActive(true);
                     transform.Find("howToPlayInside").gameObject.SetActive(true);
+                    _howToPlayInside.localPosition = new Vector2(0, -Screen.height);
+                    _howToPlayInside.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+                    _snakeText.LeanMoveLocalY(420, 0.5f).setEaseOutExpo().delay = 0.1f;
                     break;
                 case Sub.Settings:
-                    transform.Find("buttonsMainMenu").gameObject.SetActive(false);
+                    transform.Find("buttonsMainMenu").gameObject.SetActive(true);
                     transform.Find("settingsInside").gameObject.SetActive(true);
+                    _settingsInside.localPosition = new Vector2(0, -Screen.height);
+                    _settingsInside.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+                    _snakeText.LeanMoveLocalY(420, 0.5f).setEaseOutExpo().delay = 0.1f;
+                    break;
+                case Sub.Back:
+                    transform.Find("buttonsMainMenu").gameObject.SetActive(true);
+                    _snakeText.LeanMoveLocalY(380, 0.5f).setEaseOutExpo().delay = 0.1f;
+                    if (transform.Find("howToPlayInside").gameObject.activeInHierarchy)
+                    {
+                        _howToPlayInside.LeanMoveLocalY(-1400, 0.7f).setEaseOutExpo().delay = 0.1f;
+                        _howToPlayInside.gameObject.SetActive(false);
+                    }
+                    else if (transform.Find("settingsInside").gameObject.activeInHierarchy)
+                    {
+                        _settingsInside.LeanMoveLocalY(-1400, 0.7f).setEaseOutExpo().delay = 0.1f;
+                        _settingsInside.gameObject.SetActive(false);
+                    }
                     break;
             }
         }
---
+++ Assets/Scripts/UI/UIAnimation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace UI
+{
+    public class UIAnimation : MonoBehaviour
+    {
+        public void Open()
+        {
+            
+        }
+    }
+}
