ddf622db0c6cc2de34874eaceff2c9e518b23337
Blossantrape <tolik.hamer@gmail.com>
2023-09-23 16:25:14 +0300

    v 1.5

--- Assets/Scripts/General/Snake.cs
+++ Assets/Scripts/General/Snake.cs
@@ -39,7 +39,7 @@ public class Snake : MonoBehaviour
     {
         //MobileInputHandlerTouch.OnSwipe += HandleSwipe;
         _gridPosition = new Vector2Int(0, 0); // Позция змейки, 0.9 - z, чтобы объект отображался.
-        _gridMoveTimerMax = 0.3f; // Интервал движения. // Стандарт: 0.5f.
+        _gridMoveTimerMax = 0.4f; // Интервал движения. // Стандарт: 0.5f.
         _gridMoveTimer = _gridMoveTimerMax; // Так надо.
         _gridMoveDirection = Direction.Right; // Направление змеи, вправо.
 
@@ -100,6 +100,11 @@ public class Snake : MonoBehaviour
         }
     }
 
+    public void EventForGamePadHandleGrindMovement()
+    {
+        
+    }
+    
     private void HandleGrindMovement()
     {
         _gridMoveTimer += Time.deltaTime; // Обновление таймера каждый кадр.
